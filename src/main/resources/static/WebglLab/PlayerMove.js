0f377e95e8c6f9cf3f95095e9ddd78db7ee908a2
ljy <874212844@qq.com>
2019-09-11 15:12:47 +0800

    1.2:webglx

--- src/main/resources/static/WebglLab/PlayerMove.js
+++ src/main/resources/static/WebglLab/PlayerMove.js
@@ -4,16 +4,32 @@ function context(div) {
 
     create(div)
     setViewport(20);
-    var light = new THREE.AmbientLight( 0xff0000 );
+
 
     var player =  Cube(Color(123,123,123),Vector3(0,0,0));
+    var lightCube = createCube();
+
+    var plane = createPlane();
+
+    setSize(plane,Vector3(10,10,10));
+    setColor(plane,Color(123,123,123));
+    setPosition(opView.light,Vector3(0,-1,1));
 
 
     var po2 = Vector3(0,0,0);
+
+
     document.onkeydown=function(event){
 
         console.info("presskey");
+
+
         switch (event.keyCode) {
+            case 65:
+
+                console.info("adjust light source dir");
+                break;
+
             //left
             case 37:
 
@@ -40,11 +56,56 @@ function context(div) {
     console.info(JSON.stringify(po2));
 
     };
+
     addObject(player);
-    addObject(light);
+    //----------------------------------
+
+
+    //用来保存鼠标坐标信息
+
+    var mouse = new THREE.Vector2();
+    //添加鼠标移动事件，检测鼠标的移动
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+    function onDocumentMouseMove(event) {
+
+        //获取鼠标的x，y坐标
+
+        mouse.x = (event.clientX / 800) * 2 - 1;
+        mouse.y = -((event.clientY-100) /800) * 2 + 1;
+        //console.info("move"+ mouse.x+"<>"+ mouse.y+"<>"+event.clientX+"<>"+event.clientY);
+
+        return mouse;
+    }
+
+    div.onmousemove = onDocumentMouseMove;
+    //----------
+
+    var raycaster = new THREE.Raycaster();
+    div.onclick = function(){
+        raycaster.setFromCamera(mouse, opView.camera);
+        var intersects = raycaster.intersectObjects(opView.scene.children);
+        console.info(raycaster.position);
+        if (intersects.length>0)
+        {
+            intersects[ 0].object.material.color.set( 0x000000 );
+            select(intersects[0].object);
+            console.info(getSelectObject().position);
+        }
+
+    }
+
     op = function () {
 
         player.setPosition(po2);
+        setPosition(lightCube,getPosition(opView.light));
+
+
+
+
+
+
+
 
     }
 
--- src/main/resources/static/WebglLab/WebglManager.js
+++ src/main/resources/static/WebglLab/WebglManager.js
@@ -2,14 +2,37 @@
 
 var op;
 var opView;
+var selectObject;
+function setPosition(object,vector3) {
+    object.position.x = vector3.x;
+    object.position.y = vector3.y;
+    object.position.z = vector3.z;
+}
+function setRotation(object,vector3) {
+    object.rotation.x = vector3.x;
+    object.rotation.y = vector3.y;
+    object.rotation.z = vector3.z;
+}
+function setSize(object,vector3) {
+    object.scale.x = vector3.x;
+    object.scale.y = vector3.y;
+    object.scale.z = vector3.z;
+
+
+}
+function setColor(object,color) {
+    object.material.color.set(color.ox );
+}
+function setMaterial(object,material) {
+
+}
+
 
 function setViewport(z) {
     opView.camera.position.z = z;
 
 }
-function setSize(width,height) {
 
-}
 function addObject(object) {
     opView.scene.add(object);
 }
@@ -17,27 +40,36 @@ function removeObject(object) {
     opView.scene.remove(object);
 
 }
+function select(object) {
+    selectObject = object;
+}
 
+function getPosition(object) {
+
+    return Vector3(object.position.x,object.position.y,object.position.z);
+}
+function getSelectObject() {
+    return selectObject;
+}
+//创建View
 function create(div) {
 
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(45,1, 0.1, 500);
-
-
-
-
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(800, 800);
     renderer.setClearColor(Color(255,239 ,213).ox, 1.0);
     div.appendChild(renderer.domElement);
 
+    var light = new THREE.DirectionalLight( 0xff0000 );
 
     var View  = {};
+
     View.scene = scene;
     View.camera = camera;
-    View. renderer = renderer;
-
-
+    View.renderer = renderer;
+    View.light = light;
+    View.scene.add( View.light);
     opView= View;
     return View;
 }
