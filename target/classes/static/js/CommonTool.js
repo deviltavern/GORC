2b5d07f8d0180af316dae4cfe3f2289562373bfe
ljy <874212844@qq.com>
2019-09-13 21:38:52 +0800

    BR:瘦身版

--- target/classes/static/js/CommonTool.js
+++
@@ -1,9 +0,0 @@
-function seriValue2JSON(value) {
-    var tmpDic={};
-    for(var i in value.split("&")){
-        var row=value.split("&")[i];
-        tmpDic[row.split("=")[0]]=decodeURIComponent(row.split("=")[1]);
-    }
-
-    return tmpDic;
-}
--- target/classes/static/js/home_head.js
+++
@@ -1,7 +0,0 @@
-
-
-function home_head_load_home_page(){
-
-top.location.href=localStorage.getItem('loginkey');
-
-}
