087655565197a73978759df74dafe28d28e5a914
fanyuanan <740627156@qq.com>
2019-09-30 10:41:09 +0800

    增加js产生table元素

--- src/main/resources/static/js/home_head.js
+++ src/main/resources/static/js/home_head.js
@@ -5,9 +5,9 @@ function home_head_load_home_page(){
 top.location.href=localStorage.getItem('loginkey');
 
 }
-var s=document.getElementById("user_info") ;
-s.onclick=function (ev) {
-
-    console.info(s);
-    s.setAttribute("href","../score/score_page.html");
-}
+// var s=document.getElementById("user_info") ;
+// s.onclick=function (ev) {
+//
+//     console.info(s);
+//     s.setAttribute("href","../score/score_page.html");
+// }
