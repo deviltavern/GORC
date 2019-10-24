31c38b26dd3447e14d3cc25fa3e27ca7f43385e1
ljy <874212844@qq.com>
2019-10-24 17:08:35 +0800

    BR:最新

--- src/main/java/com/main/activity/signature/NameSignature.java
+++ src/main/java/com/main/activity/signature/NameSignature.java
@@ -7,9 +7,11 @@ import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import sun.misc.BASE64Decoder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -53,15 +55,15 @@ public class NameSignature {
     }
 
     @PostMapping("/onSignature")
-    public void onSignature(String name){
+    public void onSignature(String name,int code){
 
-        if(hashMap.containsKey(name)){
-            hashMap.remove(name);
+        if(hashMap.containsKey(name) ==false){
+            hashMap.put(name,new SignatureObject());
         }
 
-        hashMap.put(name,new SignatureObject());
+
         hashMap.get(name).id = name;
-        hashMap.get(name).status = 0;
+        hashMap.get(name).status = code;
         System.out.println("onSignature" + hashMap.get(name).toString());
 
 
@@ -72,6 +74,47 @@ public class NameSignature {
 
 
         return  tempPath;
+    }
+
+    /**
+     *
+     * @param key
+     * @param opName
+     * @param code
+     * 0-甲方签字，存储甲方签字后的路径
+     * 1-乙方签字，存储乙方签字后的路径
+     * @throws IOException
+     */
+    @PostMapping("/uploadSignatureHTImg")
+    public void uploadSignatureHTImg(String key,String opName,int code) throws IOException {
+
+        SignatureObject sg = hashMap.get(opName);
+
+
+        BASE64Decoder decoder = new BASE64Decoder();
+        byte[] b = decoder.decodeBuffer(key);
+        for (int i =0;i<b.length;i++){
+            if(b[i]<0){
+                b[i]+= 256;
+            }
+        }
+        String fileName = UUID.randomUUID().toString()+".png";
+        FileOutputStream os = new FileOutputStream(FolderTool.getImageCollectorPath()+fileName);
+
+        switch (code){
+            case 0:
+                sg.path1 =fileName;
+                break;
+
+            case 1:
+                sg.path2 = fileName;
+                break;
+
+        }
+        os.write(b);
+        os.flush();
+        os.close();
+
     }
 
 }
